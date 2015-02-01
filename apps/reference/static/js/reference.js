18cdaa548c4e035bbd27dc58fcfc4f18b85f56fd
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-02-01 12:57:01 +0000

    Refactor js

--- apps/reference/static/js/reference.js
+++ apps/reference/static/js/reference.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-  function hideCode() {
+  function showLanguage(language) {
+    // Hide all languages
     var codes = Dom.find('div.highlight')
     for(var i = 0; i < codes.length; i++) {
       var code = codes.item(i);
@@ -8,15 +9,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
         Dom.addClass(code, 'hidden');
       }
     }
+
+    // Show the one we want
+    codes = Dom.find('div.highlight.' + language);
+    Dom.removeClass(codes, 'hidden');
   }
 
-  var links = Dom.find('#language > a')
-  Dom.addListener(links, Dom.Event.ON_CLICK, function (event) {
-    hideCode();
-    var codes = Dom.find('div.highlight.' + event.target.dataset.language);
-    Dom.removeClass(codes, 'hidden');
-  });
+  showLanguage('ruby');
 
-  hideCode();
+  // Add listeners for language links
+  Dom.addListener(Dom.find('#language > a'), Dom.Event.ON_CLICK, function (event) {
+    showLanguage(event.target.dataset.language);
+  });
 
 });
